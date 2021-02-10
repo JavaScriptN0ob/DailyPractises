44be704b66e9bf3f825e6b87c6a5558ccba4621b
Wayne JSNoob <wenpeijs@gmail.com>
2021-02-10 15:44:05 +1100

    daily refresh and warm ups DAY2

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/* eslint-disable quote-props */
+/* eslint-disable quotes */
+module.exports = {
+  'extends': ['airbnb-base', 'plugin:react/recommended'],
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true,
+  },
+  "rules": {
+    "react/prop-types": 0,
+    "linebreak-style": 0,
+  },
+};
